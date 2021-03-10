c3def79223844ee28b7de03473ca8c46ea8187e8
LouisTsang <646168919@qq.com>
2021-03-10 22:40:47 +0800

    render raw grid

--- src/Feature/Excel.js
+++ src/Feature/Excel.js
@@ -2,14 +2,18 @@
  * Excel 实例
  */
 export default class Excel {
-  constructor (options) {
+  constructor(options) {
     this.options = options;
-    const { id } = options;
-    this.$el = document.querySelector(`#${id}`);
-    if (!this.$el) {
-      this.$el = document.createElement('canvas');
-      document.body.appendChild(this.$el);
+    const {
+      id
+    } = options;
+    const container = document.querySelector(`#${id}`);
+    console.log('container', container)
+    if (!container) {
+      throw new Error(`can't find the element: ${id}`)
     }
+    this.$el = document.createElement('canvas');
+    container.appendChild(this.$el);
     this.$ctx = this.$el.getContext('2d');
     options = options || {};
     this.monitor = null;
@@ -25,10 +29,40 @@ export default class Excel {
       editor: null
     }
   }
-  render () {
-    const { $ctx } = this;
+  render() {
+    const {
+      $ctx
+    } = this;
+    const {
+      width: w,
+      height: h
+    } = this.options;
+    console.log('h', h);
+    console.log('w', w);
+    if (h && w) {
+      this.$el.height = h;
+      this.$el.width = w;
+    }
     console.log('渲染>data>>', this.options.data, this.$ctx);
-    $ctx.rect(10, 10, 100, 100);
-    $ctx.fill();
+    const cellWidth = 80;
+    const cellHeight = 24;
+    $ctx.lineWidth = 1;
+    $ctx.strokeStyle = 'red';
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 20; j++) {
+        $ctx.strokeRect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
+      }
+    }
+    // $ctx.rect(10, 10, 100, 100);
+    // $ctx.fill();
+  }
+  // 重新绘制
+  resize (width, height, noRender) {
+    this.$el.width = `${width}px`;
+    this.$el.height = `${height}px`;
+    !noRender && this.render();
+  }
+  clear () {
+
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import Excel from './core/index.js'
 // Excel -> Frame -> Sheet -> Worker/Grid/Cell/
 const excel = new Excel({ 
   id: 'sheet',
-  data: []
+  data: [],
+  width: 1000,
+  height: 1000
 });
+// 渲染 10 x 10单元格
 excel.render();
 // excel.refresh(); // cal style
 console.log('excel:', excel);
